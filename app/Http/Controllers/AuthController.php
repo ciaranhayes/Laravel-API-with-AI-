87bc104547e76b6af419f70208cab53e0e3aea14
Ciaran Hayes <hayes.c.hayes@gmail.com>
2025-04-09 14:13:50 +0200

    Authenticated Loggin made

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request) {
+        try {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required|email|unique::users',
+                'password' => 'required|confirmed|min:8',
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json(['message' => 'New user registered'], 201);
+        } catch(\Exception $e) {
+            return response()->json(['message' => 'Registration failed'], 401);
+        }
+    }
+}
